c8f1e2b6dd53639764dd8dec34ae35b170311749
Ítalo Lima <italojonas@hotmail.com>
2024-08-12 19:45:35 -0300

    first commit

---
+++ src/app/_components/search.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { Button } from "./ui/button"
+import { SearchIcon } from "lucide-react"
+import { Input } from "./ui/input"
+import { useRouter } from "next/navigation"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/app/_components/ui/form"
+
+const formSchema = z.object({
+  title: z.string().trim().min(1, {
+    message: "Digite algo para buscar",
+  }),
+})
+
+export const Search = () => {
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  })
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershops?title=${data.title}`)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="flex gap-2">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <Input
+                  placeholder="Faça sua busca..."
+                  {...field}
+                  className="w-full"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">
+          <SearchIcon />
+        </Button>
+      </form>
+    </Form>
+  )
+}
---
+++ src/app/bookings/page.tsx
@@ -0,0 +1,81 @@
+import { getServerSession } from "next-auth"
+import { Header } from "../_components/header"
+import { authOptions } from "../_lib/auth"
+import { notFound } from "next/navigation"
+import { db } from "../_lib/prisma"
+import { BookingItem } from "../_components/booking-item"
+
+const Bookings = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (!session?.user) return notFound()
+
+  const confirmedBookings = await db.booking.findMany({
+    where: {
+      userId: (session.user as any).id,
+      date: {
+        gte: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+
+  const concludedBookings = await db.booking.findMany({
+    where: {
+      userId: (session.user as any).id,
+      date: {
+        lt: new Date(),
+      },
+    },
+    include: {
+      service: {
+        include: {
+          barbershop: true,
+        },
+      },
+    },
+    orderBy: {
+      date: "asc",
+    },
+  })
+
+  return (
+    <>
+      <Header />
+      <div className="space-y-3 p-5">
+        {confirmedBookings.length > 0 && (
+          <>
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Confirmados
+            </h2>
+            {confirmedBookings.map((booking) => (
+              <BookingItem booking={booking} />
+            ))}
+          </>
+        )}
+
+        {concludedBookings.length > 0 && (
+          <>
+            <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+              Finalizados
+            </h2>
+            {concludedBookings.map((booking) => (
+              <BookingItem booking={booking} />
+            ))}
+          </>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Bookings
